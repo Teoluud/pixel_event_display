4c171210b223ea6e14d025f8df53c5324242a6e6
Teoluud <texwiller2004@gmail.com>
2026-04-19 11:25:08 +0200

    remove redundant parser help

--- main.py
+++ main.py
@@ -7,13 +7,8 @@ if __name__ == '__main__':
     parser = OptionParser(usage= 'usage: %prog [options] run_id event_id')
     parser.add_option('-p', '--path', type=str, dest='p', default='../events/mcElectrons/csv', help='path to the import data')
     parser.add_option('-v', '--view', type=str, dest='v', default='x', help='select the view of the projection')
-    parser.add_option('-H', '--Help', dest='H', default=False, action='store_true', help='print the help and exit')
 
     (opts, args) = parser.parse_args()
-    
-    if opts.H:
-        parser.print_help()
-        sys.exit()
 
     if len(args) == 0:
         sys.exit('Please provide run and event id.')
