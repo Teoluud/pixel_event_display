a2d4da12f2c0168c16ccac0d2f9decdd13b27084
Teoluud <texwiller2004@gmail.com>
2026-04-27 14:33:33 +0200

    minor changes to scale

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import numpy as np
 def normalize_log_matrix(matrix: np.ndarray, norm: float) -> np.ndarray:
     """ Calculates the log matrix and returns it normalized to the norm value.
     """
-    log_matrix  = np.ma.log10(matrix)
-    norm_matrix = log_matrix / np.log10(norm)
+    matrix_kev   = matrix * 1e3
+    norm_kev     = norm * 1e3
+    log_matrix  = np.ma.log10(matrix_kev)
+    norm_matrix = log_matrix / np.log10(norm_kev)
     return norm_matrix
