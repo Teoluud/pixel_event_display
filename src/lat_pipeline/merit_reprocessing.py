eb255841e5fcff39a028932277c645c7f17571e9
Teoluud <texwiller2004@gmail.com>
2026-06-11 15:54:04 +0200

    minor fixes

--- src/lat_pipeline/merit_reprocessing.py
+++ src/lat_pipeline/merit_reprocessing.py
@@ -1,64 +1,76 @@
 import numpy as np
-import pandas as pd
-
 
 def merit_reprocessing(merit_vars: dict, energy: float) -> dict:
-    df0 = pd.DataFrame([merit_vars])
-    df1 = pd.DataFrame()
-    #CalNewCfpSelChiSqLog
-    df1["CalNewCfpSelChiSqLog"] = np.log10(df0["CalNewCfpSelChiSq"].values)
+    """
+    Processes a single event's merit variables using safe, pure Python math.
+    No Pandas overhead, natively handles single scalar values.
+    """
+    df1 = {}
 
-    #CalELayer10RatioES"
-    df1["CalELayer10RatioES"] = (df0["CalELayer1"].values)/(df0["CalELayer0"].values) /(-0.31*(np.log10(df0["EvtJointEnergy"].values))**2 + 4.196 * np.log10(df0["EvtJointEnergy"].values) - 10.5)
-    #TkrEnergyFracLogZS
-    EvtJointEnergy = df0["EvtJointEnergy"].values
-    TkrEnergyCorr = df0["TkrEnergyCorr"].values
-    TkrEnergyFracLogZS = []
-    for i in range(0,len(EvtJointEnergy)):
-        TkrEnergyFracLogZS.append(np.log10(TkrEnergyCorr[i]/max(1.,EvtJointEnergy[i])))
-    df1["TkrEnergyFracLogZS"] = np.array(TkrEnergyFracLogZS) -0.8*(df0["Tkr1ZDir"].values +1)
-    #Tkr1ToTTrAve
-    df1["Tkr1ToTTrAve"] = df0["Tkr1ToTTrAve"].values
-    #CalEdgeDist
-    CalXEcntr = df0["CalXEcntr"].values
-    CalYEcntr = df0["CalYEcntr"].values
-    CalEdgeDist = []
-    for i in range(0,len(CalXEcntr)):
-        CalEdgeDist.append(max(abs(CalXEcntr[i]), abs(CalYEcntr[i])))
-    df1["CalEdgeDist"] = np.array(CalEdgeDist)
-    #CalXEcntr
+    # Helper to safely grab variables (defaults to NaN if missing)
+    def get_val(key):
+        return float(merit_vars.get(key, np.nan))
+
+    # Grab everything we need for this single event
+    CalNewCfpSelChiSq = get_val("CalNewCfpSelChiSq")
+    CalELayer1 = get_val("CalELayer1")
+    CalELayer0 = get_val("CalELayer0")
+    EvtJointEnergy = energy
+    TkrEnergyCorr = get_val("TkrEnergyCorr")
+    Tkr1ZDir = get_val("Tkr1ZDir")
+    CalXEcntr = get_val("CalXEcntr")
+    CalYEcntr = get_val("CalYEcntr")
+    Tkr1X0 = get_val("Tkr1X0")
+    Tkr1Y0 = get_val("Tkr1Y0")
+    Tkr1Hits = get_val("Tkr1Hits")
+    Tkr1CoreHC = get_val("Tkr1CoreHC")
+
+    # 1. CalNewCfpSelChiSqLog
+    if CalNewCfpSelChiSq > 0:
+        df1["CalNewCfpSelChiSqLog"] = np.log10(CalNewCfpSelChiSq)
+    else:
+        df1["CalNewCfpSelChiSqLog"] = np.nan
+
+    # 2. CalELayer10RatioES
+    if CalELayer0 > 0 and EvtJointEnergy > 0:
+        logE = np.log10(EvtJointEnergy)
+        denominator = -0.31 * (logE**2) + 4.196 * logE - 10.5
+        if denominator != 0:
+            df1["CalELayer10RatioES"] = (CalELayer1 / CalELayer0) / denominator
+        else:
+            df1["CalELayer10RatioES"] = np.nan
+    else:
+        df1["CalELayer10RatioES"] = np.nan
+
+    # 3. TkrEnergyFracLogZS
+    if EvtJointEnergy > 0 and TkrEnergyCorr > 0:
+        # np.log10 requires > 0, max(1.0, E) protects the denominator
+        df1["TkrEnergyFracLogZS"] = np.log10(TkrEnergyCorr / max(1.0, EvtJointEnergy)) - 0.8 * (Tkr1ZDir + 1)
+    else:
+        df1["TkrEnergyFracLogZS"] = np.nan
+
+    # 4. Tkr1ToTTrAve
+    df1["Tkr1ToTTrAve"] = get_val("Tkr1ToTTrAve")
+
+    # 5. CalEdgeDist & Centroids
+    df1["CalEdgeDist"] = max(abs(CalXEcntr), abs(CalYEcntr))
     df1["CalXEcntr"] = CalXEcntr
     df1["CalYEcntr"] = CalYEcntr
-    #Tkr1Z0
-    df1["Tkr1Z0"] = df0["Tkr1Z0"].values
-    #TkrEdgeDist
-    Tkr1X0 = df0["Tkr1X0"].values
-    Tkr1Y0 = df0["Tkr1Y0"].values
-    TkrEdgeDist = []
-    for i in range(0,len(CalXEcntr)):
-        TkrEdgeDist.append(max(abs(Tkr1X0[i]), abs(Tkr1Y0[i])))
-    df1["TkrEdgeDist"] = np.array(TkrEdgeDist)
-    #EvtCalCsIRLn
-    df1["EvtCalCsIRLn"] = df0["EvtCalCsIRLn"].values
-    #CalZEcntr
-    df1["CalZEcntr"] = df0["CalZEcntr"].values
-    #Tkr1CoreHCRatio
-    Tkr1CoreHC = df0["Tkr1CoreHC"].values
-    Tkr1Hits = df0["Tkr1Hits"].values
-    Tkr1CoreHCRatio = []
-    for i in range(0,len(CalXEcntr)):
-        Tkr1CoreHCRatio.append(Tkr1CoreHC[i]/max(1,Tkr1Hits[i]))
-    df1["Tkr1CoreHCRatio"] = np.array(Tkr1CoreHCRatio)
-    #TkrNumStripsThinLog
-    df1["TkrNumStripsThinLog"] = np.log10(df0["TkrNumStripsThin"].values)
-    #TkrNumStripsThickLog
-    df1["TkrNumStripsThickLog"] = np.log10(df0["TkrNumStripsThick"].values)
-    #TkrNumStripsBlankLog
-    df1["TkrNumStripsBlankLog"] = np.log10(df0["TkrNumStripsBlank"].values)
 
-    #TkrTree1CalDoca68Log
-    df1["TkrTree1CalDoca68Log"] = np.log10(df0["TkrTree1CalDoca68"].values)
-    #Cal1TransRms
-    df1["CalTransRms"] = df0["CalTransRms"].values
+    # 6. Tkr1Z0
+    df1["Tkr1Z0"] = get_val("Tkr1Z0")
 
-    return df1.to_dict()
+    # 7. TkrEdgeDist
+    df1["TkrEdgeDist"] = max(abs(Tkr1X0), abs(Tkr1Y0))
+
+    # 8. EvtCalCsIRLn & CalZEcntr
+    df1["EvtCalCsIRLn"] = get_val("EvtCalCsIRLn")
+    df1["CalZEcntr"] = get_val("CalZEcntr")
+
+    # 9. Tkr1CoreHCRatio
+    if Tkr1Hits > 0:
+        df1["Tkr1CoreHCRatio"] = Tkr1CoreHC / Tkr1Hits
+    else:
+        df1["Tkr1CoreHCRatio"] = 0.0
+
+    return df1
