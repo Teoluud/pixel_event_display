4ec29e78445753ca31ad9c173fa9cefb5d061070
Teoluud <texwiller2004@gmail.com>
2026-06-11 16:21:03 +0200

    minor fixes

--- src/lat_pipeline/merit_reprocessing.py
+++ src/lat_pipeline/merit_reprocessing.py
@@ -11,7 +11,7 @@ def merit_reprocessing(merit_vars: dict) -> dict:
     def get_val(key):
         return float(merit_vars.get(key, np.nan))
 
-    # Grab everything we need for this single event
+    # Grab everything we need for a single event
     CalNewCfpSelChiSq = get_val("CalNewCfpSelChiSq")
     CalELayer1 = get_val("CalELayer1")
     CalELayer0 = get_val("CalELayer0")
@@ -25,13 +25,13 @@ def merit_reprocessing(merit_vars: dict) -> dict:
     Tkr1Hits = get_val("Tkr1Hits")
     Tkr1CoreHC = get_val("Tkr1CoreHC")
 
-    # 1. CalNewCfpSelChiSqLog
+    # CalNewCfpSelChiSqLog
     if CalNewCfpSelChiSq > 0:
         df1["CalNewCfpSelChiSqLog"] = np.log10(CalNewCfpSelChiSq)
     else:
         df1["CalNewCfpSelChiSqLog"] = np.nan
 
-    # 2. CalELayer10RatioES
+    # CalELayer10RatioES
     if CalELayer0 > 0 and EvtJointEnergy > 0:
         logE = np.log10(EvtJointEnergy)
         denominator = -0.31 * (logE**2) + 4.196 * logE - 10.5
@@ -42,34 +42,32 @@ def merit_reprocessing(merit_vars: dict) -> dict:
     else:
         df1["CalELayer10RatioES"] = np.nan
 
-    # 3. TkrEnergyFracLogZS
-    if np.isnan(TkrEnergyCorr) or np.isnan(Tkr1ZDir) or TkrEnergyCorr <= 0:
-        print(f"🚨 TKR BUG - TkrEnergyCorr: {TkrEnergyCorr} | Tkr1ZDir: {Tkr1ZDir}")
+    # TkrEnergyFracLogZS
     if EvtJointEnergy > 0 and TkrEnergyCorr > 0:
         # np.log10 requires > 0, max(1.0, E) protects the denominator
         df1["TkrEnergyFracLogZS"] = np.log10(TkrEnergyCorr / max(1.0, EvtJointEnergy)) - 0.8 * (Tkr1ZDir + 1)
     else:
         df1["TkrEnergyFracLogZS"] = np.nan
 
-    # 4. Tkr1ToTTrAve
+    # Tkr1ToTTrAve
     df1["Tkr1ToTTrAve"] = get_val("Tkr1ToTTrAve")
 
-    # 5. CalEdgeDist & Centroids
+    # CalEdgeDist & Centroids
     df1["CalEdgeDist"] = max(abs(CalXEcntr), abs(CalYEcntr))
     df1["CalXEcntr"] = CalXEcntr
     df1["CalYEcntr"] = CalYEcntr
 
-    # 6. Tkr1Z0
+    # Tkr1Z0
     df1["Tkr1Z0"] = get_val("Tkr1Z0")
 
-    # 7. TkrEdgeDist
+    # TkrEdgeDist
     df1["TkrEdgeDist"] = max(abs(Tkr1X0), abs(Tkr1Y0))
 
-    # 8. EvtCalCsIRLn & CalZEcntr
+    # EvtCalCsIRLn & CalZEcntr
     df1["EvtCalCsIRLn"] = get_val("EvtCalCsIRLn")
     df1["CalZEcntr"] = get_val("CalZEcntr")
 
-    # 9. Tkr1CoreHCRatio
+    # Tkr1CoreHCRatio
     if Tkr1Hits > 0:
         df1["Tkr1CoreHCRatio"] = Tkr1CoreHC / Tkr1Hits
     else:
